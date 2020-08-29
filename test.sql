99c8dd2f0b8cba7d9aebe11db344665dbf800a85
sohan <sohan.1si16ec113@gmail.com>
2020-08-29 14:45:48 +0530

    added sql files

---
+++ test.sql
@@ -0,0 +1,7 @@
+INSERT into routes(sender, destination, message_type, is_active)
+VALUES  ('abc1','xyz1','creaditReport',1),
+	('abc2','xyz2','creaditReport',1),
+	('abc3','xyz3','creaditReport',1),
+	('abc4','xyz4','creaditReport',1),
+	('abc5','xyz5','creaditReport',1),
+	('abc6','xyz6','creaditReport',1);
