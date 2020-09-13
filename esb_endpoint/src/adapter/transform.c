59bea1808d853f6105a8324873c38fc98dcb7c33
1ss2 <ssrivastava1099@gmail.com>
2020-09-14 00:19:58 +0530

    added transform

---
+++ esb_endpoint/src/adapter/transform.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libxml/parser.h>
+#include "../esb/bmd_parser.h"
+
+char* payloadToJson(BMD* bmd)
+{
+    char filename[50];
+    //create jsonStore folder to save files
+    //to make unique json file,add messageID
+    sprintf(filename, "../jsonStore/payload%s.json", bmd->bmd_envelope->MessageID);
+    //Create file and format payload as JSON
+    FILE *file;
+ 	file = fopen(filename,"w");
+    if(file == NULL) 
+    {
+	printf("file opening filed");
+	exit(0);
+    }
+    //write payload with json formate
+    fprintf(file,"{\n \"Payload\":\"%s\"\n}",bmd->bmd_payload->data);
+	//confirmation message
+    printf("\nPayload json File created\n");
+    fclose(file);
+    // returns json filename 
+    return strdup(filename);	
+
+}
+
+char* payloadToXml(BMD* bmd)
+{
+    char filename[50];
+    //create XMLStore folder to save files
+    //to make unique xml file, add messageID
+    sprintf(filename, "../XMLStore/payload%s.xml", bmd->bmd_envelope->MessageID);
+    //Create file and format payload as JSON
+    FILE *file;
+ 	file = fopen(filename,"w");
+    if(file == NULL) 
+    {
+	printf("file opening filed");
+	exit(0);
+    }
+    //write payload with json formate
+    fprintf(file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<?xml-stylesheet type='text/xsl'?>
+                \n<payload>%s</payload>", bmd.bmd_payload->data);
+    	
+    printf("\nPayload XML File created\n");
+    fclose(file);
+    // returns json filename 
+    return strdup(filename);
+    
+}
