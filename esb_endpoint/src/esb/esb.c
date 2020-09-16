8f2c4472bd100aa5ceda49d4f74ba21ce8c73d56
1ss2 <ssrivastava1099@gmail.com>
2020-09-16 23:24:08 +0530

    changes

--- esb_endpoint/src/esb/esb.c
+++ esb_endpoint/src/esb/esb.c
@@ -6,6 +6,7 @@
 #include <../database/database.h>
 #include <../adapter/transform.h>
 #include <../adapter/transport.h>
+BMD* b;
 /**
  * This is the main entry point into the ESB.
  * It will start processing of a BMD received at the HTTP endpoint.
@@ -69,6 +70,30 @@ int transport(char* tkey,char* address,int routeID){
     
 }
 
+int ttfunc()
+{
+         //printf("..................calculating..............\n");
+         int routeID=routeId(b->bmd_envelope->Sender,b->bmd_envelope->Destination,b->bmd_envelope->MessageType);
+
+         //printf("..................decision..............\n");
+
+        char* address=transform(transformValue(routeID),b);
+        
+        if(transport(transportKey(routeID),address,routeID)==2)
+        {
+            toAddress(transportValue(routeID),address);
+        }
+        else if(transport(transportKey(routeID),address,routeID)==1)
+        {
+            tohttp(transportValue(routeID),address);
+        }
+
+        return 0;
+}
+
+
+
+
 int process_esb_request(char* bmd_file_path) {
     int f_status = 0; // OK == 0
     printf("Handling the BMD %s\n", bmd_file_path);
@@ -85,74 +110,17 @@ int process_esb_request(char* bmd_file_path) {
      printf("BMD retrieved\n");
      if(validate_xml_file(bmd)==0)
      {
-         printf("..................calculating..............\n");
-         int routeID=routeId(bmd->bmd_envelope->Sender,bmd->bmd_envelope->Destination,bmd->bmd_envelope->MessageType);
-        // printf("transformKey:%s\n", transformKey(routeID));
-        // printf("transform_Value=%s\n",transformValue(routeID)(routeID));
-        //        printf("\n--------------assigning variables-----------------\n");
-        //  char* transform_Key=transformKey(routeID);
-        //  char* transform_Value=transformValue(routeID)(routeID);
-        //  char* transport_Key=transportKey(routeID)(routeID);
-        //  char* transport_Value=transportValue(routeID);
-        //  printf("routeID:%d\n\ntransformKey:%s\ntransform_Value:%s\n transport_Key:%s\ntransport_Value:%s\n\n",routeID, transform_Key,transform_Value,
-        //    transport_Key ,transport_Value);
-         printf("..................decision..............\n");
-         //int result=decision(bmd,transformValue(routeID),transformKey(routeID),transportValue(routeID),transportKey(routeID)(routeID));
-    // char* tfkey1="xml";
-    // char* tfkey2=transformValue(routeID);
-    // char* tpkey1=transportKey(routeID);
-    // char* tpkey2=transportValue(routeID);
-    // printf("\n\ntransport Value outside:%s\n\n",tpkey2);
-    //  printf("\n\ntransform Value outside:%s\n\n",tfkey2);
-    //   printf("\n\ntransport key outside:%s\n\n",tpkey1);
-
-
-    char* address=transform(transformValue(routeID),bmd);
-    
-    if(transport(transportKey(routeID),address,routeID)==2)
-    {
-        toAddress(transportValue(routeID),address);
-    }
-    else if(transport(transportKey(routeID),address,routeID)==1)
-    {
-        tohttp(transportValue(routeID),address);
-    }
-    
-    //  }
-    // //      if(result==0)
-    // //      {
-    // //          printf("---------------------------SUCCESS---------------------------");
-    // //      }
-
-         return 0;
+         b=bmd;
+         
+        return ttfunc();
      }
      else
      {
          printf("BMD validation failed\n");
          return 1;
      }
-    //return 0;
-    // xml2json((char*)bmd->bmd_payload->data);
-
-    // if(requestWeb()==0)
-    // printf("Data send to web:SUCCESS\n");
-    // else
-    // {
-    //     printf("Data send to web:FAILED\n");
-    // }
-
-
-    //  printf("......................As per the ID sending email...................\n");
-
-
-    //  if(sendMail((char*)bmd->bmd_envelope->Destination,"/home/pc/Desktop/git/raavi_altered/raavi/esb_endpoint/Payload.json")==0)
-    //  printf("success mail sent.....................................\n");
-    //  else
-    //  {
-    //      printf("mail sending failed......................................\n");
-    //  }
-
 
      return f_status;
 }
+
 
